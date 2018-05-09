d2ee852613e89c7457355f4ef1130f7f7d3e3c64
arunpradeep3583 <arunpradeep3583@gmail.com>
2018-05-09 19:42:26 +0530

    added code

---
+++ src/main/java/NewMaven/NewMaven/App.java
@@ -0,0 +1,13 @@
+package NewMaven.NewMaven;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
